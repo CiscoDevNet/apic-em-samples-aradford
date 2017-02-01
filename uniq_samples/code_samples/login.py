2b91964bf4b921894b1106a9997b8cfaf357ec2c
Justin Bollinger <justin@jbollinger.com>
2017-02-01 16:12:37 -0500

    Update login.py
    Additional cleanup 
    
    Cleaner host definition and return moved to try section.  It will not return a value if there is an exception.

--- uniq_samples/code_samples/login.py
+++ uniq_samples/code_samples/login.py
@@ -19,6 +19,7 @@ def login():
                 username=APIC_USER,
                 password=APIC_PASSWORD,
                 connect=True)
+        return client
     except requests.exceptions.HTTPError as exc_info:
         if exc_info.response.status_code == 401:
             print('Authentication Failed. Please provide valid username/password.')
@@ -27,9 +28,6 @@ def login():
                     code=exc_info.response.status_code,
                     reason=exc_info.response.reason))
         exit(1)
-    except requests.exceptions.ConnectionError as exec_info:
-        print('Connection aborted. Please check if the host {host} is available.'.format(host=exec_info.args[
-            0].pool.host))
+    except requests.exceptions.ConnectionError:
+        print('Connection aborted. Please check if the host {host} is available.'.format(host=APIC))
         exit(1)
-
-    return client
